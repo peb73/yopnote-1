4b0d76c34636ecce35917f1168b7533b5b963d38
peb73 <boiteaupe@gmail.com>
2012-12-27 19:15:10 +0100

    get erreur 404

--- backend/routes/folder.js
+++ backend/routes/folder.js
@@ -11,7 +11,6 @@ var mongoclient = new MongoClient(new Server(CONFIG.dbHost, CONFIG.dbPort, {nati
  */
 exports.list = function(req, res){
 
-	console.log(CONFIG);
 	mongoclient.open(function(err, mongoclient){
 		
 		if(err!=null){
@@ -41,17 +40,28 @@ exports.get = function(req, res, hash){
 
 		if(err!=null){
 			res.respond(err,500);
+			mongoclient.close();
 			return;
 		}
 
-		var dataBase = mongoclient.db(db.dbName);
+		var dataBase = mongoclient.db(CONFIG.dbName);
 		dataBase.collection(collectionName).findOne({hash: hash},function(err,doc){
 		
+			//Error
 			if(err!=null){
 				res.respond(err,500);
+				mongoclient.close();
 				return;
 			}
 
+			//No result
+			if(doc==null){
+				res.respond("No folder found",404);
+				mongoclient.close();
+				return;x
+			}
+
+			//return result
 			res.json(doc);
 			mongoclient.close();
 		});
@@ -63,5 +73,29 @@ exports.put = function(req, res, id){
 	res.respond("Not Yet Implemented",501);
 };
 exports.post = function(req, res){
-	res.respond("Not Yet Implemented",501);
+
+	if(req.body.name == null || req.body.name == ""){
+		res.respond("name param could not be null",412);
+		return;
+	}
+
+	var private = false;
+	if(req.body.private != null && (req.body.private == true || req.body.private == true))
+		private = true;
+
+	var name = req.body.name;
+
+	//TODO calcul du hash
+
+	mongoclient.open(function(err, mongoclient){
+		if(err!=null){
+			res.respond(err,500);
+			mongoclient.close();
+			return;
+		}
+
+		var dataBase = mongoclient.db(CONFIG.dbName);
+		//TODO post
+	});
+	//res.respond("Not Yet Implemented",501);
 };
