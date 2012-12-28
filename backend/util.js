8f75012349aaabb259f3c2c1e1e5ef05a09d2b35
peb73 <boiteaupe@gmail.com>
2012-12-28 10:18:28 +0100

    get hash method

--- backend/util.js
+++ backend/util.js
@@ -2,20 +2,16 @@
  * Permet de caculer le hash d'un id
  */
 function getHash(id){
-
-	return id;
-	
 	var charString = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-";
 	var charLength = charString.length;
 
 	var reste = id%charLength;
 
-	if(id/charLength>charLength){
+	if(id/charLength>1){
 		return charString[reste]+getHash((id-reste)/charLength);
 	}else{
 		return charString[reste];
 	}
-
 };
 
 exports.getHash = getHash;
